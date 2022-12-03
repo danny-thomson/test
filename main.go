74ffea5cd899f4ad9b94187c51e8ebb24077efb5
danny-thomson <dannythomsonj@gmail.com>
2022-12-03 11:45:17 +0530

    First Commit

---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import "net/http"
+
+func main() {
+	http.HandleFunc("/home", home)
+	http.ListenAndServe(":8090", nil)
+}
+
+func home(response http.ResponseWriter, request *http.Request) {
+	response.Write([]byte("testing"))
+}
